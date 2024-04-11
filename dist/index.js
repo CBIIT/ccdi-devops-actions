c3af624259919951e33179c6f7f35ab61a79b64e
Cole DeVries <colemandevries@gmail.com>
2024-04-11 12:42:17 -0400

    creating custom action

---
+++ dist/index.js
@@ -0,0 +1,92 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 396:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 716:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(396);
+const github = __nccwpck_require__(716);
+
+async function action() {
+    const token = core.getInput('TOKEN');
+    const octokit = github.getOctokit(token);
+
+    const { context = {} } = github;
+    
+
+    const title = core.getInput('title', { required: true });
+    const body = core.getMultilineInput("body", { required: true });
+
+    const { newIssue } = await octokit.rest.issues.create({
+        ...context.repo,
+        title: title,
+        body: body
+    });
+
+    core.setOutput('issue_number', newIssue.number);
+
+
+    console.log(`The event context: ${JSON.stringify(context, null, 2)}`);
+    console.log(`The new issue: ${JSON.stringify(newIssue, null, 2)}`);
+}
+
+action();
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
---
+++ src/createIssue.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function action() {
+    const token = core.getInput('TOKEN');
+    const octokit = github.getOctokit(token);
+
+    const { context = {} } = github;
+    
+
+    const title = core.getInput('title', { required: true });
+    const body = core.getMultilineInput("body", { required: true });
+
+    const { newIssue } = await octokit.rest.issues.create({
+        ...context.repo,
+        title: title,
+        body: body
+    });
+
+    core.setOutput('issue_number', newIssue.number);
+
+
+    console.log(`The event context: ${JSON.stringify(context, null, 2)}`);
+    console.log(`The new issue: ${JSON.stringify(newIssue, null, 2)}`);
+}
+
+action();
