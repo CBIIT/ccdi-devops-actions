2a7b26d70316b9c6225d7937a2f135fbc343ebcd
Cole DeVries <colemandevries@gmail.com>
2024-04-11 16:44:11 -0400

    updating the function code

--- src/createIssue.js
+++ src/createIssue.js
@@ -1,28 +1,20 @@
+import fs from 'fs/promises';
 import { Octokit } from "@octokit/action";
 
 
-async function getPlanContent( client, org, repo, artifact ) {
-    const { data: plan } = await client.actions.listWorkflowRunArtifacts({
-        owner: org,
-        repo: repo,
-        run_id: process.env.GITHUB_RUN_ID,
-        name: artifact
-    });
-
-    const { data: content } = await client.actions.downloadArtifact({
-        owner: org,
-        repo: repo,
-        artifact_id: plan.artifacts[0].id,
-        archive_format: 'zip'
-    });
-
-    return Buffer.from(content).toString('base64');
+async function readFile( file ) {
+    try {
+        const data = await fs.readFile(file, { encoding: 'utf8' });
+        return data;
+    } catch (err) {
+        console.error(`Error reading plan file: ${err}`);
+    }
 }
 
+async function createNewIssue( client, org, repo ) {
+    const issueTitle = `${process.env.GITHUB_ENV}-${process.env.GITHUB_RUN_ID}-${process.env.GITHUB_RUN_ATTEMPT}`
+    const issueBody = await readFile( 'tfplan' );
 
-async function createNewIssue( client, org, repo, artifact ) {
-    const issueTitle = `${process.env.GITHUB_ENV}-${process.env.GITHUB_RUN_ID}`
-    const issueBody = await getPlanContent( client, org, repo, artifact );
     const { data: issue } = await client.issues.create({
         owner: org,
         repo: repo,
@@ -35,13 +27,12 @@ async function createNewIssue( client, org, repo, artifact ) {
 }
 
 
-async function run() {
+async function action() {
     const client = new Octokit();
-    const artifact = process.env.INPUT_ARTIFACT;
     const [org, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    const issueNumber = await createNewIssue( client, org, repo, issueBody );
 
-    const issueNumber = await createNewIssue( client, org, repo, artifact );
-    console.log(`Issue number: ${issueNumber}`);
+    return issueNumber;
 }
 
-run();
+action();
