930e2695b7fd9f6362f98df7cb73e4384bb345c4
Cole DeVries <colemandevries@gmail.com>
2024-04-12 10:18:47 -0400

    updated the readfile function

--- src/createIssue.js
+++ src/createIssue.js
@@ -9,31 +9,36 @@ async function readFile( file ) {
     });
 }
 
-async function createNewIssue( client, org, repo ) {
-    const issueTitle = `${process.env.GITHUB_ENV}-${process.env.GITHUB_RUN_ID}-${process.env.GITHUB_RUN_ATTEMPT}`
-    const issueBody = await readFile( 'README.txt' );
+async function run() {
+    const content = await readFile( 'README.txt' );
+    console.log( content );
+}
 
-    const { data: issue } = await client.issues.create({
-        owner: org,
-        repo: repo,
-        title: issueTitle,
-        body: issueBody
-    });
+// async function createNewIssue( client, org, repo ) {
+//     const issueTitle = `${process.env.GITHUB_ENV}-${process.env.GITHUB_RUN_ID}-${process.env.GITHUB_RUN_ATTEMPT}`
+//     const issueBody = await readFile( 'README.txt' );
+
+//     const { data: issue } = await client.issues.create({
+//         owner: org,
+//         repo: repo,
+//         title: issueTitle,
+//         body: issueBody
+//     });
     
-    console.log(`Issue created: ${issue.html_url}`)
-    return issue.number;
-}
+//     console.log(`Issue created: ${issue.html_url}`)
+//     return issue.number;
+// }
 
 
-async function action() {
+// async function action() {
 
 
 
-    const client = new Octokit();
-    const [org, repo] = process.env.GITHUB_REPOSITORY.split("/");
-    const issueNumber = await createNewIssue( client, org, repo );
+//     const client = new Octokit();
+//     const [org, repo] = process.env.GITHUB_REPOSITORY.split("/");
+//     const issueNumber = await createNewIssue( client, org, repo );
 
-    return issueNumber;
-}
+//     return issueNumber;
+// }
 
-action();
+// action();
